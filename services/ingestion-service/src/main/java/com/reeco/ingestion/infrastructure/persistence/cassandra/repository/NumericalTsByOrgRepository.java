b3ab63a7620fa4986592183191cd6ffd717ade9c
tungduong <tungduonghmd@gmail.com>
2021-11-06 02:33:02 +0700

    update cassandra agg

--- services/ingestion-service/src/main/java/com/reeco/ingestion/infrastructure/persistence/cassandra/repository/NumericalTsByOrgRepository.java
+++ services/ingestion-service/src/main/java/com/reeco/ingestion/infrastructure/persistence/cassandra/repository/NumericalTsByOrgRepository.java
@@ -1,16 +1,12 @@
 package com.reeco.ingestion.infrastructure.persistence.cassandra.repository;
 
-import com.reeco.ingestion.infrastructure.persistence.cassandra.entity.NumericalStatByOrg;
 import com.reeco.ingestion.infrastructure.persistence.cassandra.entity.NumericalTsByOrg;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
