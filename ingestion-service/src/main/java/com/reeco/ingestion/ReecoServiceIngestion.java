d61f0a9cc3f0239cbe2e7b692592fc74c190cc02
tungduong <tungduonghmd@gmail.com>
2021-12-15 00:49:24 +0700

    feat: update timezone

--- ingestion-service/src/main/java/com/reeco/ingestion/ReecoServiceIngestion.java
+++ ingestion-service/src/main/java/com/reeco/ingestion/ReecoServiceIngestion.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class ReecoServiceIngestion {
 	public static void main(String[] args) {
 		SpringApplication.run(ReecoServiceIngestion.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Ho_Chi_Minh")));
 	}
 
 }
