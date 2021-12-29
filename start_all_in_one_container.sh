bc79bd09bacf015bd6f89f6de10b16e433cd98ad
tungduong <tungduonghmd@gmail.com>
2021-12-30 00:24:49 +0700

    feat: update docker-compose infras

---
+++ start_all_in_one_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+echo "Starting docker compose ..."
+sudo docker-compose -f docker-compose-infras.yml up -d
+
+sleep 1
+echo "Init Cassandra Schema ..."
+docker exec -it cassandra bash -c "cqlsh -f /opt/initdb/schema.cql"
+
+sleep 1
+echo "Starting Reeco serivces ..."
+sudo docker-compose -f docker-compose-all-in-one.yml up --build -d
+
+echo "Done!"
+
+exit 0
