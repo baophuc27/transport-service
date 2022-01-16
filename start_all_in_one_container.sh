9cc0c5dedf86be0bba7a5e5e4128fe271b65ced6
nhanvu39 <nhanvu1998@gmail.com>
2022-01-16 21:36:39 +0700

    add init db

--- start_all_in_one_container.sh
+++ start_all_in_one_container.sh
@@ -3,7 +3,7 @@ set -ex
 CASSANDRA_USER=$1
 CASSANDRA_PASSWORD=$2
 echo "Starting docker compose ..."
-sudo docker-compose -f docker-compose-infras.yml up -d
+sudo docker-compose -f docker-compose-infras.yml up --build -d
 
 sleep 1
 echo "Init Cassandra Schema ..."
@@ -11,7 +11,7 @@ sudo docker exec -it cassandra bash -c "cqlsh -u ${CASSANDRA_USER} -p ${CASSANDR
 
 sleep 1
 echo "Starting Reeco services ..."
-sudo docker-compose -f docker-compose-run-all-in-one.yml up -d
+sudo docker-compose -f docker-compose-build-all-in-one.yml up --build -d
 
 echo "Done!"
 
