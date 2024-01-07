54fd34d06b9bc5a541e49f1b5a65dedb432711e9
Phuc <phucphucbao2710@gmail.com>
2024-01-07 23:23:05 +0700

    [f] disable MQTT when can not connect to broker

--- transport-service/src/main/java/com/reeco/transport/infrastructure/MQTTMessagePublisher.java
+++ transport-service/src/main/java/com/reeco/transport/infrastructure/MQTTMessagePublisher.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.net.ssl.SSLSocketFactory;
 import java.util.Arrays;
@@ -21,6 +22,8 @@ public class MQTTMessagePublisher {
     @Autowired
     private final ObjectMapper objectMapper;
 
+    private boolean USE_MQTT = true;
+
     private final MqttClient client;
     public MQTTMessagePublisher(ObjectMapper objectMapper) throws MqttException {
         this.objectMapper = objectMapper;
@@ -28,37 +31,45 @@ public class MQTTMessagePublisher {
                 "tcp://localhost:3000", // serverURI in format: "protocol://name:port"
                 "admin-dev", // ClientId
                 new MemoryPersistence()); // Persistence
-        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-        mqttConnectOptions.setUserName("admin");
-        mqttConnectOptions.setPassword("admin123".toCharArray());
+        try{
+
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setUserName("admin");
+            mqttConnectOptions.setPassword("admin123".toCharArray());
 //        mqttConnectOptions.setSocketFactory(SSLSocketFactory.getDefault()); // using the default socket factory
-        client.connect(mqttConnectOptions);
-        client.setCallback(new MqttCallback() {
+                client.connect(mqttConnectOptions);
+                client.setCallback(new MqttCallback() {
 
-            @Override
-            public void connectionLost(Throwable cause) { // Called when the client lost the connection to the broker
-                System.out.println(cause);
-            }
+                    @Override
+                    public void connectionLost(Throwable cause) { // Called when the client lost the connection to the broker
+                        System.out.println(cause);
+                    }
 
-            @Override
-            public void messageArrived(String topic, MqttMessage message) {
-                System.out.println(topic + ": " + Arrays.toString(message.getPayload()));
-            }
+                    @Override
+                    public void messageArrived(String topic, MqttMessage message) {
+                        System.out.println(topic + ": " + Arrays.toString(message.getPayload()));
+                    }
 
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) { // Called when a outgoing publish is complete
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken token) { // Called when a outgoing publish is complete
 //                System.out.println("complete");
-            }
-        });
+                    }
+                });
+
+        }
+        catch (Exception e){
+            log.info("MQTT has not bean created.");
+            USE_MQTT = false;
+        }
+
     }
 
     public void publish(String topic, DataRecordMessage message) throws MqttException, JsonProcessingException {
 
-        log.info("Publish message: {} to: {}",message,topic);
-        String data = objectMapper.writeValueAsString(message);
-        client.publish(topic,data.getBytes(UTF_8),1,false);
-
-
-
+        if (USE_MQTT){
+            log.info("Publish message: {} to: {}",message,topic);
+            String data = objectMapper.writeValueAsString(message);
+            client.publish(topic,data.getBytes(UTF_8),1,false);
+        }
     }
 }
